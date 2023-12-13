546892b9c224e67048689bd0827c299cbf9217d5
xcode <xcode@stdlib.in>
2023-12-13 15:14:40 +0900

    1. update.sh 변경

--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
-git pull
-pnpm run build
+# 호스트 머신에서 실행한다.
+
+#!/bin/bash
+set -e;
+
+CONT_NM=$1;
+
+docker exec -it "$CONT_NM" git pull;
+docker exec -it "$CONT_NM" pnpm run build;
+docker restart "$CONT_NM";
